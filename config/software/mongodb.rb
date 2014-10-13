a818f914b10df3abe9dd2f69b250037a11ec6b95
Nick Muerdter <nick.muerdter@nrel.gov>
2014-10-13 01:56:02 -0600

    Bump dependencies

--- config/software/mongodb.rb
+++ config/software/mongodb.rb
@@ -1,13 +1,27 @@
 name "mongodb"
 default_version "2.6.4"
 
-if Ohai['kernel']['machine'] =~ /x86_64/
-  source :url => "http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-#{version}.tgz",
-         :md5 => "484f07082803e21691ece39fa9e1292b"
+version "2.6.4" do
+  if(Ohai['kernel']['machine'] =~ /x86_64/)
+    source :md5 => "484f07082803e21691ece39fa9e1292b"
+  else
+    source :md5 => "99baf442a3fa6bfa58f01a750155713f"
+  end
+end
+
+version "2.6.5" do
+  if(Ohai['kernel']['machine'] =~ /x86_64/)
+    source :md5 => "933fb038082f6eeb8182964bd51880c1"
+  else
+    source :md5 => "08bbceac04b072f596a6f7593c1241ae"
+  end
+end
+
+source :url => "http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-#{version}.tgz"
+
+if(Ohai['kernel']['machine'] =~ /x86_64/)
   relative_path "mongodb-linux-x86_64-#{version}"
 else
-  source :url => "http://fastdl.mongodb.org/linux/mongodb-linux-i686-#{version}.tgz",
-         :md5 => "99baf442a3fa6bfa58f01a750155713f"
   relative_path "mongodb-linux-i686-#{version}"
 end
 
