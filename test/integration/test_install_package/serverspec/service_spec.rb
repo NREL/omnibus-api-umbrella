666e73ced10366a1dcd5dc479702ffbcc3a9aee1
Nick Muerdter <nick.muerdter@nrel.gov>
2015-02-08 08:49:27 -0700

    Add more integration tests for the package installer.

--- test/integration/test_install_package/serverspec/service_spec.rb
+++ test/integration/test_install_package/serverspec/service_spec.rb
@@ -58,12 +58,109 @@ describe "api-umbrella" do
   end
 
   it "fails immediately when startup script is called as an unauthorized user" do
-    `sudo -u api-umbrella-deploy /etc/init.d/api-umbrella start`
-    expect($!.to_i).not_to eql(0)
+    expect(`sudo -u api-umbrella-deploy /etc/init.d/api-umbrella start 2>&1`).to include("Could not open")
+    expect($?.to_i).not_to eql(0)
   end
 
   it "allows the deploy user to execute api-umbrella as root" do
-    expect(`sudo -u api-umbrella-deploy sudo -n api-umbrella status`).to include("is running")
-    expect(`sudo -u api-umbrella-deploy sudo -n /etc/init.d/api-umbrella status`).to include("is running")
+    expect(`sudo -u api-umbrella-deploy sudo -n api-umbrella status 2>&1`).to include("is running")
+    expect(`sudo -u api-umbrella-deploy sudo -n /etc/init.d/api-umbrella status 2>&1`).to include("is running")
+  end
+
+  it "can be stopped and started again" do
+    expect(`/etc/init.d/api-umbrella stop 2>&1`).to include("OK")
+    expect($?.to_i).to eql(0)
+
+    expect(service("api-umbrella")).to_not be_running
+    expect(process("supervisord")).to_not be_running
+    expect(`/etc/init.d/api-umbrella status 2>&1`).to include("is stopped")
+
+    expect(`/etc/init.d/api-umbrella start 2>&1`).to include("OK")
+    expect($?.to_i).to eql(0)
+    expect(service("api-umbrella")).to be_running
+    expect(process("supervisord")).to be_running
+  end
+
+  it "symlinks the main api-umbrella binary" do
+    subject = file("/usr/bin/api-umbrella")
+    expect(subject).to be_symlink
+    expect(subject).to be_owned_by("root")
+    expect(subject).to be_grouped_into("root")
+    expect(subject).to be_linked_to("/opt/api-umbrella/bin/api-umbrella")
+  end
+
+  it "installs a init.d file" do
+    subject = file("/etc/init.d/api-umbrella")
+    expect(subject).to be_file
+    expect(subject).to be_mode(755)
+    expect(subject).to be_owned_by("root")
+    expect(subject).to be_grouped_into("root")
+  end
+
+  it "installs a logrotate.d file" do
+    subject = file("/etc/logrotate.d/api-umbrella")
+    expect(subject).to be_file
+    expect(subject).to be_mode(644)
+    expect(subject).to be_owned_by("root")
+    expect(subject).to be_grouped_into("root")
+  end
+
+  it "installs a sudoers.d file" do
+    subject = file("/etc/sudoers.d/api-umbrella")
+    expect(subject).to be_file
+    expect(subject).to be_mode(644)
+    expect(subject).to be_owned_by("root")
+    expect(subject).to be_grouped_into("root")
+  end
+
+  it "installs a api-umbrella.yml file" do
+    subject = file("/etc/api-umbrella/api-umbrella.yml")
+    expect(subject).to be_file
+    expect(subject).to be_mode(644)
+    expect(subject).to be_owned_by("root")
+    expect(subject).to be_grouped_into("root")
+  end
+
+  it "sets up the api-umbrella user" do
+    subject = user("api-umbrella")
+    expect(subject).to exist
+    expect(subject).to belong_to_group("api-umbrella")
+    expect(subject).to have_home_directory("/opt/api-umbrella")
+    expect(subject).to have_login_shell("/sbin/nologin")
+  end
+
+  it "sets up the api-umbrella-deploy user's home directory and empty ssh keys file" do
+    subject = user("api-umbrella-deploy")
+    expect(subject).to exist
+    expect(subject).to belong_to_group("api-umbrella-deploy")
+    expect(subject).to have_home_directory("/home/api-umbrella-deploy")
+    expect(subject).to have_login_shell("/bin/bash")
+
+    subject = file("/home/api-umbrella-deploy")
+    expect(subject).to be_directory
+    expect(subject).to be_mode(700)
+    expect(subject).to be_owned_by("api-umbrella-deploy")
+    expect(subject).to be_grouped_into("api-umbrella-deploy")
+
+    subject = file("/home/api-umbrella-deploy/.ssh")
+    expect(subject).to be_directory
+    expect(subject).to be_mode(700)
+    expect(subject).to be_owned_by("api-umbrella-deploy")
+    expect(subject).to be_grouped_into("api-umbrella-deploy")
+
+    subject = file("/home/api-umbrella-deploy/.ssh/authorized_keys")
+    expect(subject).to be_file
+    expect(subject).to be_mode(600)
+    expect(subject).to be_owned_by("api-umbrella-deploy")
+    expect(subject).to be_grouped_into("api-umbrella-deploy")
+    expect(subject.content).to eql("")
+  end
+
+  it "symlinks the log directory" do
+    subject = file("/var/log/api-umbrella")
+    expect(subject).to be_symlink
+    expect(subject).to be_owned_by("root")
+    expect(subject).to be_grouped_into("root")
+    expect(subject).to be_linked_to("/opt/api-umbrella/var/log")
   end
 end
